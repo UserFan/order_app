b61eee7cf8af311060eb6b494588948541150ea0
Fanis <habfanis@mail.ru>
2017-09-20 00:40:09 +0300

    Refresh header

--- db/schema.rb
+++ db/schema.rb
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20170918072330) do
     t.string "full_name", default: "", null: false
     t.string "mobile", default: "", null: false
     t.integer "position", default: 0, null: false
+    t.integer "role", default: 1, null: false
     t.boolean "admin", default: false, null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -48,7 +49,8 @@ ActiveRecord::Schema.define(version: 20170918072330) do
     t.datetime "locked_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "role", default: 1, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
