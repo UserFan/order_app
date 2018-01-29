bd22c39d7cd623f4500d32610ca32552a76c5f50
Fanis <habfanis@gmail.com>
2018-01-30 01:08:30 +0300

    RefreshCash

--- db/schema.rb
+++ db/schema.rb
@@ -170,12 +170,6 @@ ActiveRecord::Schema.define(version: 20180126073628) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "scaleses", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "scaners", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
