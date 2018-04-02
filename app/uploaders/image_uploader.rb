1f69ac1f18a6d916c4bfde98e29d50ee4f45999a
Fanis <habfanis@gmail.com>
2018-04-02 16:54:59 +0300

    UploderCompresion

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -6,6 +6,8 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
+  #include CarrierWave::ImageOptimizer
+  process quality: 50
 
 
   # Choose what kind of storage to use for this uploader:
