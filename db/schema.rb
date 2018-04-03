062d3d805fab312b4dbaa98c74c648d49d692234
Fanis <habfanis@gmail.com>
2018-04-03 23:52:07 +0300

    UpdateViewOrder

--- db/schema.rb
+++ db/schema.rb
@@ -254,12 +254,6 @@ ActiveRecord::Schema.define(version: 20180403065639) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "scaleses", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "scaners", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
