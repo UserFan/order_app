aecb2577eaa8845ebe607cd49672dddef028e033
Fanis <habfanis@gmail.com>
2018-05-31 12:17:53 +0300

    RefreshSimCard

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -1,7 +1,6 @@
 class Provider < ApplicationRecord
   has_many :item_communications, dependent: :restrict_with_error
   has_many :sim_cards, dependent: :restrict_with_error
-  has_many :shop_communication, through: :item_communications
-  has_many :shop, through: :shop_communication
+  
   validates :name, presence: true
 end
--- app/models/sim_card.rb
+++ app/models/sim_card.rb
@@ -1,7 +1,7 @@
 class SimCard < ApplicationRecord
   has_many :item_communications, dependent: :restrict_with_error
   belongs_to :provider
-  has_one :shop, through: :provider
-
+  has_many :shop_communications, through: :item_communications
+  has_many :shops, through: :shop_communications
   validates :sim_number, :simphone_number, :provider_id, presence: true
 end
