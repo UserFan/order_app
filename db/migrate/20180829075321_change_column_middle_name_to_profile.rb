290b99831dd3d2246d5c52e8a1650d672881e2f8
Fanis <habfanis@gmail.com>
2018-09-07 14:53:46 +0300

    AddRecordMigration

--- db/migrate/20180829075321_change_column_middle_name_to_profile.rb
+++
@@ -1,5 +0,0 @@
-class ChangeColumnMiddleNameToProfile < ActiveRecord::Migration[5.1]
-  def change
-    change_column_null :profiles, :middle_name, true
-  end
-end
---
+++ db/migrate/20180907113804_add_record_profile.rb
@@ -0,0 +1,11 @@
+class AddRecordProfile < ActiveRecord::Migration[5.1]
+  def change
+    unless Profile.find_by(user_id: 1).present?
+      Profile.create!(user_id: 1,
+                      surname: Администратор,
+                      first_nmae: системы,
+                      mobile: '+70000000000',
+                      position_id: 1)
+    end
+  end
+end
