df4ee857663e85383a9a93978f0010babccf3569
Fanis <habfanis@gmail.com>
2018-09-10 13:14:12 +0300

    RefreshSimcardorSystemUnitEditView

--- db/migrate/20180907120102_add_all_user_profile.rb
+++ db/migrate/20180907120102_add_all_user_profile.rb
@@ -1,18 +1,20 @@
 class AddAllUserProfile < ActiveRecord::Migration[5.1]
   def change
     User.find_each do |user|
-      if user.id == 1
-        Profile.create!(user_id: user.id,
-                        surname: 'Администратор',
-                        first_name: 'системы',
-                        mobile: '+70000000000',
-                        position_id: 1)
-      else
-        Profile.create!(user_id: user.id,
-                        surname: 'User',
-                        first_name: 'User_name',
-                        mobile: '+70000000000',
-                        position_id: 1)
+      unless user.profile.present?
+        if user.id == 1
+          Profile.create!(user_id: user.id,
+                          surname: 'Администратор',
+                          first_name: 'системы',
+                          mobile: '+70000000000',
+                          position_id: 1)
+        else
+          Profile.create!(user_id: user.id,
+                          surname: 'User',
+                          first_name: 'User_name',
+                          mobile: '+70000000000',
+                          position_id: 1)
+        end
       end
     end
   end
