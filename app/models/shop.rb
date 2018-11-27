bf0694de07f79d689999b42251e2403ab5b7d7ca
Fanis <habfanis@gmail.com>
2018-11-27 08:29:32 +0300

    ShopUpdate

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,6 +1,8 @@
 class Shop < ApplicationRecord
   mount_uploader :photo, ImageUploader
 
+  scope :structural, -> { where(structural_unit: true) }
+
   geocoded_by :address
   reverse_geocoded_by :latitude, :longitude
   after_validation :geocode, :if => lambda{ |obj| obj.address_changed? }
