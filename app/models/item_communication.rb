1f21cae46c345878005dbdc0e4d0c64c49832079
Fanis <habfanis@gmail.com>
2019-03-26 15:15:03 +0300

    FixSimLogModel

--- app/models/item_communication.rb
+++ app/models/item_communication.rb
@@ -19,41 +19,30 @@ class ItemCommunication < ApplicationRecord
   private
 
   def add_sim_log
-    SimLog.create!(sim_card_id: sim_card_id,
-                   shop_id: shop.id,
-                   date_start: DateTime.now) if sim_card_id.present?
+    sim_log_change(nil, nil, true) if sim_card_id.present?
   end
-
-  def sim_log_change(params=nil)
-    unless params.nil?
-      sim_log = SimLog.find_by(sim_card_id: params)
-      sim_log.update_attributes(date_end: DateTime.now) if sim_log.present?
-    end
-  end
-
 
   def update_sim_log
     if sim_card_id_changed?
-<<<<<<< HEAD
       if (sim_card_id_was.nil? && sim_card_id.present?)
-        SimLog.create!(sim_card_id: sim_card_id,
-                       shop_id: shop.id,
-                       date_start: DateTime.now)
+        sim_log_change(nil, nil, true)
       elsif (sim_card_id_was.present? && sim_card_id.nil?)
-        sim_log_change(sim_card_id_was)
+        sim_log_change(sim_card_id_was, shop.id)
       else
-        sim_log_change(sim_card_id_was)
-        SimLog.create!(sim_card_id: sim_card_id,
-                       shop_id: shop.id,
-                       date_start: DateTime.now)
-=======
-      unless sim_card_id_was.nil?
-        sim_log = SimLog.find_by(sim_card_id: sim_card_id_was,
-                                 shop_id: shop.id,
-                                 date_end: nil)
-        sim_log.update_attributes(date_end: DateTime.now) if sim_log.present?
->>>>>>> 11cfd5eb86b8476d08bce7fc2471debd5704b62e
+        sim_log_change(sim_card_id_was, shop.id, true)
       end
     end
+  end
+
+  def sim_log_change(sim_id=nil, shop_id=nil, add=false)
+    unless (sim_id.nil? && shop_id.nil?)
+      sim_log = SimLog.find_by(sim_card_id: sim_id,
+                               shop_id: shop_id,
+                               date_end: nil)
+      sim_log.update_attributes(date_end: DateTime.now) if sim_log.present?
+    end
+    SimLog.create!(sim_card_id: sim_card_id,
+                   shop_id: shop.id,
+                   date_start: DateTime.now) if add
   end
 end
